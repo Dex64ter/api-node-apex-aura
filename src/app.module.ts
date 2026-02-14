68c0ef49aed653fbc55aa30e630c0569ba2bd366
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2026-02-14 20:55:35 -0300

    feat: adicionando tratamento de erro para falhas de conexao com o banco de dados, e adicionando .env.exemple para novos integrantes adicionarem seus proprios .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    DatabaseModule,
     UsersModule,
-    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
   ],
   controllers: [AppController],
   providers: [AppService],
