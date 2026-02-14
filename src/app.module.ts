e9a909416d98579df4a536809c1ad95dcfdbb25b
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2026-02-14 20:25:07 -0300

    feat: atualiza a versão da imagem do MongoDB e adiciona módulo de configuração

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
-    MongooseModule.forRoot(
-      'mongodb://admin:password@localhost:27017/api_db?authSource=admin',
-    ),
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
   ],
   controllers: [AppController],
   providers: [AppService],
