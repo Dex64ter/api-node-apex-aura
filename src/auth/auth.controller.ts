d169c0e72cfb2c57eef2b9465ed38b4bfc289499
Davi José Cunha Santos <davijosantos.dj@gmail.com>
2026-03-11 19:30:54 -0300

    teste de auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body() body: { email: string; password: string }) {
+    const { email, password } = body;
+    return this.authService.signUp(email, password);
+  }
+
+  @Post('/signin')
+  signIn(@Body() body: { email: string; password: string }) {
+    const { email, password } = body;
+    return this.authService.signIn(email, password);
+  }
+}
