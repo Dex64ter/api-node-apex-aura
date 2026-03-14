4d54de8b9128ddebc405856f4b56e6d9b43536f0
Davi José Cunha Santos <davijosantos.dj@gmail.com>
2026-03-14 01:18:54 -0300

    jwt authentication and some entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
     DatabaseModule,
     UsersModule,
     AuthModule,
+    TeamsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
